3560fad27df1557beef0063bb2d6a92d23905475
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-21 22:35:09 +0530

    refactor: settings and drawer

---
+++ lib/pages/settings.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: Text('Settings'),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        margin: EdgeInsets.all(25),
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //dark mode
+            Text("Dark Mode"),
+        
+            //switch
+            // CupertinoSwitch(
+            //   value: Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+            //   onChanged: (value) =>
+            //     Provider.of<ThemeProvider>(context, listen: false).toggleTheme(),
+            //   activeColor: Theme.of(context).colorScheme.primary,
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
