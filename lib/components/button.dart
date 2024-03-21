e9f17e4cedda3a9b5b6d4384ec533260af67ef65
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-21 21:30:54 +0530

    modify: button to gradient button

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,14 +4,26 @@ class MyButton extends StatelessWidget {
   final Function()? onTap;
   final String text;
 
-  const MyButton({super.key, required this.text, required this.onTap});
+  const MyButton({Key? key, required this.text, required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        decoration: BoxDecoration(color: Theme.of(context).colorScheme.secondary),
+        padding: EdgeInsets.all(20),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Theme.of(context).colorScheme.secondary,
+              Theme.of(context).colorScheme.tertiary,
+            ],
+          ),
+        ),
         child: Center(
           child: Text(text),
         ),
--- lib/themes/lightMode.dart
+++ lib/themes/lightMode.dart
@@ -5,7 +5,7 @@ ThemeData lightMode = ThemeData(
     background: Color.fromARGB(255, 254, 254, 250),
     primary: Color.fromARGB(255, 88, 41, 0),
     secondary: Color.fromARGB(255, 252, 190, 118),
-    tertiary: Color.fromARGB(255, 115, 79, 48),
+    tertiary: Color.fromARGB(255, 174, 68, 12),
     inversePrimary: Colors.grey.shade900,
   )
 );
