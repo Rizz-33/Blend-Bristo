9ee5360c9ee7453274c0e8daeaed6342cd3671cd
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-21 15:27:38 +0530

    refactor: dark mode

---
+++ lib/themes/darkMode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: Color.fromARGB(255, 28, 28, 28),
+    primary: Color.fromARGB(255, 87, 134, 255),
+    secondary: Color.fromARGB(255, 16, 37, 101),
+    tertiary: Colors.black,
+    inversePrimary: Colors.grey.shade300,
+  )
+);
