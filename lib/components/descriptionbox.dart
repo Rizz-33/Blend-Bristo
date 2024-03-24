85dc9976a56fa311da8d9179f0637611bd3186a4
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-24 15:27:16 +0530

    refactor: descrption box

--- lib/components/descriptionbox.dart
+++ lib/components/descriptionbox.dart
@@ -5,24 +5,34 @@ class MyDescrptionBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    var myPrimaryTextStyle = TextStyle(
+      color: Theme.of(context).colorScheme.primary
+    );
+    var mySecondaryTextStyle = TextStyle(
+      color: Theme.of(context).colorScheme.tertiary
+    );
+
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: Theme.of(context).colorScheme.tertiary)
+        border: Border.all(color: Theme.of(context).colorScheme.tertiary),
+        borderRadius: BorderRadius.circular(16)
       ),
       padding: EdgeInsets.all(25),
+      margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
             children: [
-              Text('\$6.99'),
-              Text('Delivery Fee'),
+              Text('\$6.99', style: myPrimaryTextStyle,),
+              Text('Delivery Fee', style: mySecondaryTextStyle,),
             ],
           ),
           Column(
             children: [
-              Text('15-20 min'),
-              Text('Delivery Time'),
+              Text('15-20 min', style: myPrimaryTextStyle,),
+              Text('Delivery Time', style: mySecondaryTextStyle,),
             ],
           ),
         ],
