0f5431ec8e2797c80ea254f1f057ccb27ed43176
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-25 20:36:32 +0530

    refactor: food page

--- lib/pages/food.dart
+++ lib/pages/food.dart
@@ -1,10 +1,16 @@
+import 'package:blend_bristo/components/button.dart';
 import 'package:blend_bristo/models/food.dart';
 import 'package:flutter/material.dart';
 
 class FoodPage extends StatefulWidget {
   final Food food;
+  final Map<Addon, bool> selectedAddons = {};
 
-  const FoodPage({super.key, required this.food});
+  FoodPage({Key? key, required this.food}) : super(key: key) {
+    for (Addon addon in food.availableAddon) {
+      selectedAddons[addon] = false;
+    }
+  }
 
   @override
   State<FoodPage> createState() => _FoodPageState();
@@ -13,10 +19,118 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    widget.food.imagePath,
+                    height: 200,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 25.0, right: 25, left: 25),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.food.name,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          widget.food.price.toString() + " LKR",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Theme.of(context).colorScheme.primary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Text(
+                          widget.food.description,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Divider(color: Theme.of(context).colorScheme.primary,),
+                        SizedBox(height: 10,),
+                        Text(
+                          'Add-ons',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        ListView.builder(
+                          padding: EdgeInsets.zero,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: widget.food.availableAddon.length,
+                          itemBuilder: (context, index) {
+                            Addon addon = widget.food.availableAddon[index];
+                            return CheckboxListTile(
+                              title: Text(addon.name),
+                              subtitle: Text(
+                                addon.price.toString()+" LKR",
+                                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                              ),
+                              value: widget.selectedAddons[addon],
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  widget.selectedAddons[addon] = value!;
+                                });
+                              },
+                            );
+                          },
+                        ),
+                        Divider(color: Theme.of(context).colorScheme.primary,),
+                        SizedBox(height: 80,),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 40,
+            left: 10,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 25.0),
+              child: MyButton(
+                text: 'Add To Cart',
+                onTap: () {
+                  // Your button onTap functionality
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
