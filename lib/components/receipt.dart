a370d7693288b9743bda96bcd152dd13212680f6
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-26 21:38:07 +0530

    refator: receipt

--- lib/components/receipt.dart
+++ lib/components/receipt.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MyReceipt extends StatelessWidget {
-  const MyReceipt({super.key});
+  const MyReceipt({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    DateTime currentTime = DateTime.now();
+    DateTime estimatedDeliveryTime = currentTime.add(Duration(minutes: 15));
+
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 50),
@@ -15,22 +18,24 @@ class MyReceipt extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text("Thank you for your order !"),
-              SizedBox(height: 25,),
+              SizedBox(height: 25),
               Container(
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Theme.of(context).colorScheme.secondary,
                   ),
-                  borderRadius: BorderRadius.circular(16)
+                  borderRadius: BorderRadius.circular(16),
                 ),
                 padding: EdgeInsets.all(25),
                 child: Consumer<Restaurant>(
                   builder: (context, restaurant, child) =>
-                    Text(restaurant.displayCartReceipt()),
+                      Text(restaurant.displayCartReceipt()),
                 ),
               ),
-              SizedBox(height: 25,),
-              Text("Estimated delivery time is: ")
+              SizedBox(height: 25),
+              Text(
+                "Estimated delivery time is: ${estimatedDeliveryTime.hour}:${estimatedDeliveryTime.minute}",
+              ),
             ],
           ),
         ),
--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -438,6 +438,8 @@ class Restaurant extends ChangeNotifier{
     receipt.writeln();
     receipt.writeln("Total Items: ${getTotalItemCount()}");
     receipt.writeln("Total Price: ${_formatPrice(getTotalPrice())}");
+    receipt.writeln();
+    receipt.writeln("Delivering to: $deliveryAddress");
 
     return receipt.toString();
   }
