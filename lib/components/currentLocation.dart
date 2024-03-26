224e0968a4c2d1b971be3fe603aaee9197824231
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-26 21:31:38 +0530

    refactor: current location

--- lib/components/currentLocation.dart
+++ lib/components/currentLocation.dart
@@ -1,33 +1,48 @@
+import 'package:blend_bristo/models/restaurant.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyCurrentLocation extends StatelessWidget {
-  const MyCurrentLocation({super.key});
-
-  void openLocationSerchBox(BuildContext context){
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('Your Location'),
-        content: TextField(
-          decoration: InputDecoration(
-            hintText: "Enter Address.."
-          ),
-        ),
-        actions: [
-          MaterialButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('Save'),
-          ),
-          MaterialButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('Cancel'),
-          ),
-        ],
-      ));
-  }
+  const MyCurrentLocation({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController textController = TextEditingController();
+
+    void openLocationSearchBox(BuildContext context) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Your Location'),
+          content: TextField(
+            controller: textController,
+            decoration: InputDecoration(
+              hintText: "Enter Address..",
+            ),
+          ),
+          actions: [
+            MaterialButton(
+              onPressed: () {
+                Navigator.pop(context);
+                textController.clear();
+              },
+              child: Text('Cancel'),
+            ),
+            MaterialButton(
+              onPressed: () {
+                String newAddress = textController.text;
+                Provider.of<Restaurant>(context, listen: false).updateDeliveryAddress(newAddress); // Used Provider.of instead of context.read
+                Navigator.pop(context);
+                textController.clear();
+              },
+              child: Text('Save'),
+            ),
+            
+          ],
+        ),
+      );
+    }
+
     return Padding(
       padding: const EdgeInsets.all(25.0),
       child: Column(
@@ -38,17 +53,22 @@ class MyCurrentLocation extends StatelessWidget {
             style: TextStyle(color: Theme.of(context).colorScheme.primary),
           ),
           GestureDetector(
-            onTap:  () => openLocationSerchBox(context),
+            onTap: () => openLocationSearchBox(context),
             child: Row(
               children: [
-                Text(
-                  'address',
-                  style: TextStyle(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+                Consumer<Restaurant>(
+                  builder: (context, restaurant, child) => Text(
+                    restaurant.deliveryAddress,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-                Icon(Icons.keyboard_arrow_down_rounded)
+                Icon(Icons.keyboard_arrow_down_rounded),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
