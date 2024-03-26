373d29770d289932b8bd35dd68a48cd70718faf9
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-26 19:18:05 +0530

    feat: auth gate for login signup or home page

---
+++ lib/services/auth/authGate.dart
@@ -0,0 +1,28 @@
+import 'package:blend_bristo/pages/home.dart';
+import 'package:blend_bristo/services/auth/login_or_register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          //user logged in
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+
+          //user is not logged in
+          else {
+            return const LoginOrRegister();
+          }
+        },
+      ),
+    );
+  }
+}
